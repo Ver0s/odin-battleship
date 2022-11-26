1ff574b6dc0b0ea7a0323d79f07868f4791b5298
Arek <arek.stachyra41@gmail.com>
2022-11-26 19:25:56 +0100

    conigure style loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,10 @@ module.exports = {
 				test: /\.m?js$/,
 				exclude: /(node_modules|bower_components)/,
 				use: 'babel-loader',
-				// use: {
-				// 	loader: 'babel-loader',
-				// 	options: {
-				// 		presets: ['@babel/preset-env'],
-				// 	},
-				// },
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
 			},
 		],
 	},
