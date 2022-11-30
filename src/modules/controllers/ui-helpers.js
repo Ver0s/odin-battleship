e08bb5542489548c69e508c506db1aa2b25eff74
Arek <arek.stachyra41@gmail.com>
2022-11-30 18:03:40 +0100

    move smaller UI functions to file with helpers

---
+++ src/modules/controllers/ui-helpers.js
@@ -0,0 +1,89 @@
+export const getCellCoords = (cell) =>
+	cell.dataset.coords.split('').map((coordStr) => Number(coordStr));
+
+export const getCellElement = (board, rowPos, colPos) =>
+	[...board.children].find(
+		(cell) => cell.dataset.coords === `${rowPos}${colPos}`
+	);
+
+export const generateBoard = (boardElement) => {
+	for (let i = 0; i < 100; i++) {
+		const cell = document.createElement('div');
+		cell.classList.add('cell');
+		cell.dataset.coords = i < 10 ? `0${i}` : i;
+		boardElement.appendChild(cell);
+	}
+};
+
+export const showShips = (board, boardElement) => {
+	[...boardElement.children].forEach((cell) => {
+		const [rowPos, colPos] = getCellCoords(cell);
+
+		if (board.isPosShip(rowPos, colPos)) {
+			cell.classList.add('ship');
+		}
+	});
+};
+
+export const highlightShipHover = (board, cell, shipLength, isVertical) => {
+	const [rowPos, colPos] = getCellCoords(cell);
+
+	if (isVertical) {
+		for (let i = 0; i < shipLength; i++) {
+			getCellElement(board, rowPos + i, colPos).classList.add(
+				'ship-hover'
+			);
+		}
+	} else {
+		for (let i = 0; i < shipLength; i++) {
+			getCellElement(board, rowPos, colPos + i).classList.add(
+				'ship-hover'
+			);
+		}
+	}
+};
+
+export const clearShipHover = (board) => {
+	[...board.children].forEach((cell) => {
+		cell.classList.remove('ship-hover');
+	});
+};
+
+export const updateBoards = (player, computer) => {
+	const playerBoardElement = document.querySelector('.player-board');
+	const computerBoardElement = document.querySelector('.computer-board');
+
+	[...playerBoardElement.children].forEach((cell) => {
+		const [rowPos, colPos] = getCellCoords(cell);
+
+		if (
+			player.getBoard().isPosShip(rowPos, colPos) &&
+			player.getBoard().isPosHit(rowPos, colPos)
+		) {
+			cell.classList.add('hit');
+		}
+		if (
+			!player.getBoard().isPosShip(rowPos, colPos) &&
+			player.getBoard().isPosHit(rowPos, colPos)
+		) {
+			cell.classList.add('miss');
+		}
+	});
+
+	[...computerBoardElement.children].forEach((cell) => {
+		const [rowPos, colPos] = getCellCoords(cell);
+
+		if (
+			computer.getBoard().isPosShip(rowPos, colPos) &&
+			computer.getBoard().isPosHit(rowPos, colPos)
+		) {
+			cell.classList.add('hit');
+		}
+		if (
+			!computer.getBoard().isPosShip(rowPos, colPos) &&
+			computer.getBoard().isPosHit(rowPos, colPos)
+		) {
+			cell.classList.add('miss');
+		}
+	});
+};
