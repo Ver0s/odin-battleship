061bdebcf8aadef31b90162e1c4559141d418f93
Arek <arek.stachyra41@gmail.com>
2022-12-07 20:58:24 +0100

    group logic into functions

--- src/modules/controllers/ui-controller.js
+++ src/modules/controllers/ui-controller.js
@@ -8,85 +8,127 @@ const UIController = () => {
 	const setupBoardContainerElement =
 		document.querySelector('.setup-container');
 	const setupBoardElement = document.querySelector('.setup-board');
-	uiHelpers.generateBoard(setupBoardElement);
 	const playerBoardElement = document.querySelector('.player-board');
 	const computerBoardElement = document.querySelector('.computer-board');
+	const restartGameBtn = document.querySelector('#restart-game');
+
+	const initInterface = () => {
+		uiHelpers.generateBoard(setupBoardElement);
+		uiHelpers.generateBoard(playerBoardElement);
+		uiHelpers.generateBoard(computerBoardElement);
+		uiHelpers.hideBoard(playerBoardElement);
+		uiHelpers.hideBoard(computerBoardElement);
+	};
 
 	const showPlayerBoards = () => {
 		uiHelpers.hideElement(setupBoardContainerElement);
-		uiHelpers.generateBoard(playerBoardElement);
-		uiHelpers.generateBoard(computerBoardElement);
+		uiHelpers.hideBoard(playerBoardElement, false);
+		uiHelpers.hideBoard(computerBoardElement, false);
 		uiHelpers.showShips(game.getPlayer().getBoard(), playerBoardElement);
 	};
 
-	const handleCellClick = (e) => {
-		const { target } = e;
+	const handleCellClick = (event) => {
+		const { target } = event;
+		if (!target.matches('.cell')) return;
 
-		if (target.matches('.cell')) {
-			const [rowPos, colPos] = uiHelpers.getCellCoords(target);
+		const [rowPos, colPos] = uiHelpers.getCellCoords(target);
+		game.playRound(rowPos, colPos);
 
-			game.playRound(rowPos, colPos);
-			uiHelpers.updateBoards(game.getPlayer(), game.getComputer());
+		if (game.checkForWin()) {
+			uiHelpers.showEndGameModal(game.getWinnerName());
+		}
+		uiHelpers.updateBoards(game.getPlayer(), game.getComputer());
+	};
+
+	const handleShipPlacement = (event) => {
+		const { target } = event;
+		if (!target.matches('.cell')) return;
+
+		const [rowPos, colPos] = uiHelpers.getCellCoords(target);
+		game.placeShips(rowPos, colPos, isVertical);
+		uiHelpers.showShips(game.getPlayer().getBoard(), setupBoardElement);
+
+		if (game.arePlayerShipsEmpty()) {
+			showPlayerBoards();
 		}
 	};
 
-	const handleShipPlacement = (e) => {
-		const { target } = e;
-		if (target.matches('.cell')) {
-			const [rowPos, colPos] = uiHelpers.getCellCoords(target);
-			game.placeShips(rowPos, colPos, isVertical);
-			uiHelpers.showShips(game.getPlayer().getBoard(), setupBoardElement);
+	const handleRestartGame = () => {
+		// Hide the player and computer boards
+		uiHelpers.hideElement(playerBoardElement);
+		uiHelpers.hideElement(computerBoardElement);
 
-			if (game.arePlayerShipsEmpty()) showPlayerBoards();
-		}
+		// Hide the end game modal
+		uiHelpers.hideEndGameModal();
+
+		// Restart the game
+		game.restartGame();
+
+		// Show the setup board container
+		uiHelpers.hideElement(setupBoardContainerElement, false);
+
+		// Clear the setup, player, and computer boards
+		uiHelpers.clearBoard(setupBoardElement);
+		uiHelpers.clearBoard(playerBoardElement);
+		uiHelpers.clearBoard(computerBoardElement);
 	};
 
-	setupBoardElement.addEventListener('click', handleShipPlacement);
+	const handleShipHover = (event) => {
+		const { target } = event;
+		if (!target.matches('.cell')) return;
 
-	setupBoardElement.addEventListener('mouseover', (e) => {
-		const { target } = e;
-		if (target.matches('.cell')) {
-			const [rowPos, colPos] = uiHelpers.getCellCoords(target);
-			if (
-				!game
-					.getPlayer()
-					.getBoard()
-					.isLegalPlacement(
-						rowPos,
-						colPos,
-						game.getCurrentShip().getLength(),
-						isVertical
-					)
-			) {
-				uiHelpers.highlightShipHoverError(
-					setupBoardElement,
-					target,
-					game.getCurrentShip().getLength(),
-					isVertical
-				);
-				return;
-			}
-
-			uiHelpers.highlightShipHover(
-				setupBoardElement,
-				target,
+		const [rowPos, colPos] = uiHelpers.getCellCoords(target);
+		const isLegalPlacement = game
+			.getPlayer()
+			.getBoard()
+			.isLegalPlacement(
+				rowPos,
+				colPos,
 				game.getCurrentShip().getLength(),
 				isVertical
 			);
-		}
-	});
 
-	computerBoardElement.addEventListener('click', handleCellClick);
+		uiHelpers.highlightShipHover(
+			setupBoardElement,
+			target,
+			game.getCurrentShip().getLength(),
+			isVertical,
+			!isLegalPlacement
+		);
+	};
 
-	setupBoardElement.addEventListener('mouseout', () => {
-		uiHelpers.clearShipHover(setupBoardElement);
-	});
+	const handleShipOut = () => uiHelpers.clearShipHover(setupBoardElement);
 
 	window.addEventListener('keydown', (e) => {
 		if (e.key === 'r') {
 			isVertical = !isVertical;
 		}
 	});
+
+	document
+		.querySelector('#end-game-modal')
+		.addEventListener('cancel', (e) => {
+			e.preventDefault();
+		});
+
+	const setupBoardListener = () => {
+		setupBoardElement.addEventListener('click', handleShipPlacement);
+		setupBoardElement.addEventListener('mouseover', handleShipHover);
+		setupBoardElement.addEventListener('mouseout', handleShipOut);
+	};
+
+	const computerBoardListener = () => {
+		computerBoardElement.addEventListener('click', handleCellClick);
+	};
+
+	const restartGameListener = () => {
+		restartGameBtn.addEventListener('click', handleRestartGame);
+	};
+
+	initInterface();
+	setupBoardListener();
+	computerBoardListener();
+	restartGameListener();
 };
 
 export default UIController;
